a700a7255df8301a28dd212ec2b6036fda298a02
max bellomy <fmbellomy@quantumgarbage.com>
2022-02-15 23:16:37 -0600

    huh

--- src/raymond/hardware.rs
+++ src/raymond/hardware.rs
@@ -25,22 +25,17 @@ impl Hardware {
 #[cfg(target_arch = "arm")]
 impl Raymond for Hardware {
     fn connect(&mut self) {
-        let mut options = LedMatrixOptions::new();
-        options.set_hardware_mapping("adafruit-hat");
-        self.matrix = LedMatrix::new(Some(options), None).unwrap();
-        self.canvas = self.matrix.offscreen_canvas();
+        // this doesn't do anything
     }
     fn send_frame(&mut self, f: &mut Frame) {
         for x in 0..32 {
             for y in 0..32 {
-                for y in 0..32 {
-                    self.canvas.set(
-                        x,
-                        y,
-                        &f.this[(x as usize, y as usize)].to_led_color().clone(),
-                    );
-                    self.matrix.swap(self.canvas);
-                }
+                self.canvas.set(
+                    x,
+                    y,
+                    &f.this[(x as usize, y as usize)].to_led_color().clone(),
+                );
+                self.matrix.swap(self.canvas);
             }
         }
     }

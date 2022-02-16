d08917bb3306d4f822f3f9cdcbd78fa2f8aa797d
max bellomy <fmbellomy@quantumgarbage.com>
2022-02-15 20:56:34 -0600

    yeah man

--- src/raymond/hardware.rs
+++ src/raymond/hardware.rs
@@ -1,6 +1,33 @@
-struct Hardware {}
-impl Hardware {
-    pub fn connect() {}
-    pub fn send_frame() {}
-    pub fn close() {}
+#[cfg(target_arch = "arm")]
+use rpi_led_matrix::{LedCanvas, LedMatrix, LedMatrixOptions};
+
+use crate::frame::Frame;
+#[cfg(target_arch = "arm")]
+struct Hardware {
+    matrix: LedMatrix,
+    canvas: LedCanvas,
+}
+#[cfg(target_arch = "arm")]
+impl Raymond for Hardware {
+    fn connect(&mut self) {
+        let options = LedMatrixOptions::new();
+        options.set_hardware_mapping("adafruit-hat");
+        self.matrix = LedMatrix::new(Some(options), None).unwrap();
+        self.canvas = self.matrix.offscreen_canvas();
+    }
+    fn send_frame(&mut self, f: &Frame) {
+        for x in 0..32 {
+            for y in 0..32 {            for y in 0..32 {
+
+                self.canvas.set(x, y, f.this.as_row_major()[y * 32 + x]);
+                self.canvas.swap(self.canvas);
+            }
+        }
+    }
+    fn close(&mut self) {
+        self.canvas.clear();
+        self.matrix.swap(canvas);
+        drop(canvas);
+        drop(matrix);
+    }
 }

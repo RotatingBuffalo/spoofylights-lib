3636a1b738559d686634f57b8412f93e79805bb9
max bellomy <fmbellomy@quantumgarbage.com>
2022-02-15 23:09:14 -0600

    no way this actually works

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use spoofylightslib::frame::{algos::Algos, Frame};
+#[cfg(target_arch = "arm")]
+use spoofylightslib::raymond::hardware::Hardware;
 use spoofylightslib::raymond::javasimulator::JavaSimulator;
 use spoofylightslib::raymond::Raymond;
 use std::{thread, time};
 
 fn main() {
-    let framebuffer = time::Duration::from_millis(1000 / 25);
-    let mut f = Frame::new(Algos::hue_wave);
-    let mut target = JavaSimulator::new();
-    target.connect();
-    for i in 0..3000 {
-        target.send_frame(&mut f);
-        thread::sleep(framebuffer);
-        Frame::tick(&mut f, i * 3);
+    #[cfg(target_arch = "arm")]
+    {
+        let framebuffer = time::Duration::from_millis(1000 / 25);
+        let mut f = Frame::new(Algos::hue_wave);
+        let mut target = Hardware::new();
+        target.connect();
+        for i in 0..3000 {
+            target.send_frame(&mut f);
+            thread::sleep(framebuffer);
+            Frame::tick(&mut f, i * 3);
+        }
+        target.close();
     }
-    target.close();
+    #[cfg(target_arch = "x86_64")]
+    {}
 }
